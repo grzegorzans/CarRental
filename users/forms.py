4ce64beb519ea7924c7b75704ac66d5242cafad8
grzegorzans <14612@eppuz.edu.pl>
2025-05-06 15:36:10 +0200

    User registration form

--- users/forms.py
+++ users/forms.py
@@ -5,3 +5,4 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = models.User
         exclude = ['id']
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'phone', 'birth_date', 'identity_document_type', 'identity_document_no']
